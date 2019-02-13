e78cc834dffbf087ebb64585263a3b4232f284d8
Christofer Flores <christofer@michelada.io>
2019-02-13 12:58:45 -0600

    Make some moddifications on the layout, add the filters and the arrange of the selected pokemons

--- frontend/src/components/HeroPanel.js
+++ frontend/src/components/HeroPanel.js
@@ -17,31 +17,31 @@ export default class HeroPanel extends React.Component {
       <div className="nes-container is-rounded is-dark with-title">
         <h2 className="title">Characters</h2>
         <div className="pure-g spread-elements">
-          <div className="pure-u-1-1 pure-u-sm-1-3 pure-u-md-1-3 pure-u-lg-1-3 pure-u-xl-1-6">
+          <div className="pure-u-1 pure-u-sm-1-3 pure-u-md-1-8">
             <div className="tile">
               <div className="card">
                 <figure className="">
-                  <img src="https://via.placeholder.com/150" alt="Trulli"  />
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
                   <figcaption>Wolverine.</figcaption>
                 </figure>
               </div>
             </div>
           </div>
-          <div className="pure-u-1-1 pure-u-sm-1-3 pure-u-md-1-3 pure-u-lg-1-3 pure-u-xl-1-6">
+          <div className="pure-u-1 pure-u-sm-1-3 pure-u-md-1-8">
             <div className="tile">
               <div className="card">
                 <figure className="">
-                  <img src="https://via.placeholder.com/150" alt="Trulli"  />
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
                   <figcaption>Wolverine.</figcaption>
                 </figure>
               </div>
             </div>
           </div>
-          <div className="pure-u-1-1 pure-u-sm-1-3 pure-u-md-1-3 pure-u-lg-1-3 pure-u-xl-1-6">
+          <div className="pure-u-1 pure-u-sm-1-3 pure-u-md-1-8">
             <div className="tile">
               <div className="card">
                 <figure className="">
-                  <img src="https://via.placeholder.com/150" alt="Trulli"  />
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
                   <figcaption>Wolverine.</figcaption>
                 </figure>
               </div>
@@ -53,38 +53,113 @@ export default class HeroPanel extends React.Component {
       <div className="nes-container is-rounded with-title is- with-margin-top is-centered">
         <h2 className="title">Selected Characters</h2>
         <div className="pure-g center-elements">
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
         </div>
         <div className="pure-g">
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
         </div>
         <div className="pure-g">
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
-            <div className="pure-u-sm-1-4"><p>Selected</p></div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
+            <div className="pure-u-sm-1-4">
+              <figure className="">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Trulli"  />
+                <figcaption>Wolverine.</figcaption>
+              </figure>
+            </div>
         </div>
       </div>
      </div>
      <div className="nes-container with-title with-margin-top">
-      <h2 className="title">Play!</h2>
+      <h2 className="title">Filters</h2>
       <div className="is-centered">
         <div className="pure-g">
-            <div className="pure-u-sm-1-3">
-              <button className="nes-btn is-success with-margin-right">Cool</button>
+          <div className="pure-u-sm-1-3">
+            <div>
+              <label>
+                <input type="checkbox" className="nes-checkbox" name="answer[]" />
+                <span>Fire Type</span>
+              </label>
             </div>
-            <div className="pure-u-sm-1-3">
-              <button className="nes-btn is-warning">Bored</button>
-            </div>
-            <div className="pure-u-sm-1-3">
-              <button className="nes-btn is-error">Angry</button>
-            </div>
+          </div>
+          <div className="pure-u-sm-1-3">
+            <label>
+              <input type="checkbox" className="nes-checkbox" name="answer[]" />
+              <span>Water Type</span>
+            </label>
+          </div>
+          <div className="pure-u-sm-1-3">
+            <label>
+              <input type="checkbox" className="nes-checkbox" name="answer[]" />
+              <span>Poison Type</span>
+            </label>
+          </div>
+          <hr className="with-margin-top"/>
+          <div className="with-margin-top">
+            <button className="nes-btn is-success">Get the pokemons</button>
+          </div>
         </div>
       </div>
      </div>
