32aff36b66857c64c6bcb54f37371e9c8cd96854
Christofer Flores <christofer@michelada.io>
2019-03-25 17:25:12 -0600

    Add the pipenv files needed, fix the test and add the icons to the frontend app

--- frontend/src/components/Filters.js
+++ frontend/src/components/Filters.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import unionImg from '../assets/union.png'
+import diffImg from '../assets/difference.png'
+import interImg from '../assets/intersection.png'
+import symDifImg from '../assets/symmetric_difference.png'
 
 export default class Filters extends React.Component {
 
@@ -28,16 +32,24 @@ export default class Filters extends React.Component {
         <div className="is-centered">
           <div className="pure-g">
             <div className="pure-u-sm-1">
-              <button className={ currentAction === 'union' ? 'nes-btn selected' : 'nes-btn' } onClick={() => {this.callAction('union')}}>Union</button>
+              <button className={ currentAction === 'union' ? 'nes-btn selected' : 'nes-btn' } onClick={() => {this.callAction('union')}}>
+                <img src={unionImg} alt="union" width="50" height="50"/>
+              </button>
             </div>
             <div className="pure-u-sm-1 with-margin-top">
-              <button className={ currentAction === 'intersection' ? 'nes-btn is-primary selected' : 'nes-btn is-primary' } onClick={() => {this.callAction('intersection')}}>Intersection</button>
+              <button className={ currentAction === 'intersection' ? 'nes-btn is-primary selected' : 'nes-btn is-primary' } onClick={() => {this.callAction('intersection')}}>
+                <img src={interImg} alt="intersection" width="50" height="50"/>
+              </button>
             </div>
             <div className="pure-u-sm-1 with-margin-top">
-              <button className={ currentAction === 'difference' ? 'nes-btn is-success selected' : 'nes-btn is-success' } onClick={() => {this.callAction('difference')}}>Difference</button>
+              <button className={ currentAction === 'difference' ? 'nes-btn is-success selected' : 'nes-btn is-success' } onClick={() => {this.callAction('difference')}}>
+                <img src={diffImg} alt="difference" width="50" height="50" className="flip-img"/>
+              </button>
             </div>
             <div className="pure-u-sm-1 with-margin-top">
-              <button className={ currentAction === 'symmetric_difference' ? 'nes-btn is-warning selected' : 'nes-btn is-warning' } onClick={() => {this.callAction('symmetric_difference')}}>Symmetric Difference</button>
+              <button className={ currentAction === 'symmetric_difference' ? 'nes-btn is-warning selected' : 'nes-btn is-warning' } onClick={() => {this.callAction('symmetric_difference')}}>
+                <img src={symDifImg} alt="symmetric_difference" width="50" height="50"/>
+              </button>
             </div>
             <hr className="with-margin-vertical" />
             <a href="#" onClick={this.reset}>
